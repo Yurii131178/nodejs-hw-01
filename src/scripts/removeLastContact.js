77cd09e966713b67f6e7719bab86512ca6263dc6
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-25 00:20:33 +0300

    add function removeLastContact_test_generate 5_removeLast_ 4 left

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const currentContacts = await readContacts();
+
+    if (currentContacts.length === 0) {
+      console.log('No contacts to remove');
+      return;
+    }
+
+    const updatedContacts = currentContacts.slice(0, -1);
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.log('Error while last Contact removal', error);
+  }
+};
 
 removeLastContact();
