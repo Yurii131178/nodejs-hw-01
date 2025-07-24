2dd64998266c6f3e428c3fa6e27d52f50607b99e
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-24 20:17:04 +0300

    add read and write functions add prettier

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts);
+
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('contacts are successfully written to file');
+  } catch (error) {
+    console.log('File writing error', error);
+    throw error;
+  }
+};
