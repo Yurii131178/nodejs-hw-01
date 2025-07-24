af4a16a9b2c6d698f68c4b64086435b3a0bc877e
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-24 22:13:03 +0300

    test addOneContact function_test is successful

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,13 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const currentContacts = await readContacts();
+  const newContact = createFakeContact();
+  currentContacts.push(newContact);
+  await writeContacts(currentContacts);
+  console.log(`Successfully written one contact.`);
+};
 
 addOneContact();
