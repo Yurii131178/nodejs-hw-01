a4079a4bd052e5cd2b602209f4b4440c3f429c2b
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-09 19:25:54 +0300

    update constant

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,7 +1,12 @@
-import { fileURLToPath } from 'node:url';
+// import { fileURLToPath } from 'node:url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// export const PATH_DB = path.resolve(__dirname, '../db/db.json');
+
+// import path from 'node:path';
+// export const PATH_DB = path.join('src', 'db', 'db.json');
+
 import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export const PATH_DB = path.resolve(__dirname, '../db/db.json');
+export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
